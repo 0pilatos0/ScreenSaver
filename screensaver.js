6c715f490bb02b546068e87589ccf471b7758ec3
paulvanderlei <paul.vanderlei@wics.nl>
2022-12-14 08:57:11 +0100

    fixed snow

--- screensaver.js
+++ screensaver.js
@@ -73,11 +73,6 @@
 
     document.body.appendChild(snow);
 
-    const snow = document.createElement("div");
-    snow.id = "snow";
-
-    document.body.appendChild(snow);
-
 
     /*defining animations*/
     const roll = () => {
@@ -163,36 +158,6 @@
 
 
 
-
-    var date = new Date();
-
-    //quick refresh of all colors to White/orange scheduled every hour
-    if (date.getHours() % 2 == 0 && date.getMinutes() == 0) {
-        roll();
-    }
-
-    //if it's christmas, run santa every 30 minutes
-    if (date.getMonth() + 1 == 12 && date.getDate() >= 6) {
-        if (date.getMinutes() % 30 == 0) {
-            santaCycle();
-        }
-        document.getElementById('snow').style.display = "block";
-        christmas.style.display = "block";
-    } else {
-        document.getElementById('snow').style.display = "none";
-        christmas.style.display = "none";
-    }
-
-    if (date.getMonth() + 1 == 12 && date.getDate() == 5) {
-        if (date.getMinutes() % 30 == 0) {
-            sinterklaasCycle();
-        }
-    }
-
-    setInterval(() => {
-        spawnSnowCSS(snowflakes_count);
-        spawn_snow(snowflakes_count);
-    },300);
     /*timouts for animations*/
     setInterval(() => {
         var date = new Date();
@@ -210,7 +175,6 @@
             document.getElementById('snow').style.display = "block";
             christmas.style.display = "block";
         } else {
-            document.getElementById('snow').style.display = "none";
             document.getElementById('snow').style.display = "none";
             christmas.style.display = "none";
         }
@@ -283,50 +247,6 @@
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-
-    // Create style for snowflake
-    function spawnSnowCSS(snow_density = 200) {
-        let snowflake_name = "snowflake";
-        let rule = ``;
-        if (typeof base_css !== 'undefined') {
-            rule = base_css;
-        }
-
-        for (let i = 1; i < snow_density; i++) {
-            let random_x = Math.random() * 100; // vw
-            let random_offset = random_range(-100000, 100000) * 0.0001; // vw;
-            let random_x_end = random_x + random_offset;
-            let random_x_end_yoyo = random_x + (random_offset / 2);
-            let random_yoyo_time = random_range(30000, 80000) / 100000;
-            let random_yoyo_y = random_yoyo_time * 100; // vh
-            let random_scale = Math.random();
-            let fall_duration = random_range(10, 30) * 1; // s
-            let fall_delay = random_int(30) * -1; // s
-            let opacity_ = Math.random();
-
-            rule += `
-        .${snowflake_name}:nth-child(${i}) {
-            opacity: ${opacity_};
-            transform: translate(${random_x}vw, -10px) scale(${random_scale});
-            animation: fall-${i} ${fall_duration}s ${fall_delay}s linear infinite;
-        }
-
-        @keyframes fall-${i} {
-            ${random_yoyo_time*100}% {
-                transform: translate(${random_x_end}vw, ${random_yoyo_y}vh) scale(${random_scale});
-            }
-
-            to {
-                transform: translate(${random_x_end_yoyo}vw, 100vh) scale(${random_scale});
-            }
-            
-        }
-        `
-        }
-
-        add_css(rule);
     }
 
 
